e2ff5e40f650af87aed7a1dce858b5208c8fc473
alemser <lemser.alessandro@gmail.com>
2015-01-19 17:37:05 -0200

    Ajuste de quotes

--- app/scripts/directives/loadingSpinnerDirective.js
+++ app/scripts/directives/loadingSpinnerDirective.js
@@ -1,10 +1,10 @@
 angular.module('sampleNgFrontendApp')
     .directive('loadingSpinner', ['$rootScope', function ($rootScope) {
         return function($scope, element, attrs) {
-            $scope.$on("showLoading", function() {
+            $scope.$on('showLoading', function() {
                 return element.fadeIn();
             });
-            return $scope.$on("hideLoading", function() {
+            return $scope.$on('hideLoading', function() {
                 return element.fadeOut();
             });
         };
--- app/scripts/interceptors/loadingSpinnerInterceptor.js
+++ app/scripts/interceptors/loadingSpinnerInterceptor.js
@@ -4,18 +4,18 @@ angular.module('sampleNgFrontendApp')
         return {
             request: function(config) {
                 loadingCount++;
-                $rootScope.$broadcast("showLoading");
+                $rootScope.$broadcast('showLoading');
                 return config || $q.when(config);
             },
             response: function(response) {
                 if ((--loadingCount) === 0) {
-                    $rootScope.$broadcast("hideLoading");
+                    $rootScope.$broadcast('hideLoading');
                 }
                 return response || $q.when(response);
             },
             responseError: function(response) {
                 if (!(--loadingCount)) {
-                    $rootScope.$broadcast("hideLoading");
+                    $rootScope.$broadcast('hideLoading');
                 }
                 $rootScope.$broadcast('serverError', response.data);
                 return $q.reject(response);
